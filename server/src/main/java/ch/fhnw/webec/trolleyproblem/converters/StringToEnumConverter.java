b5bf5cf09400844547b1ed08ee76f39ccb111477
Livio Brunner <livio.brunner.lb1@gmail.com>
2024-05-28 18:45:40 +0200

    remove uneeded enum

--- server/src/main/java/ch/fhnw/webec/trolleyproblem/converters/StringToEnumConverter.java
+++ server/src/main/java/ch/fhnw/webec/trolleyproblem/converters/StringToEnumConverter.java
@@ -1,14 +1,14 @@
 package ch.fhnw.webec.trolleyproblem.converters;
 
-import ch.fhnw.webec.trolleyproblem.dtos.Position;
+import ch.fhnw.webec.trolleyproblem.entities.Directional;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 
-public class StringToEnumConverter implements Converter<String, Position> {
+public class StringToEnumConverter implements Converter<String, Directional> {
     @Override
-    public Position convert(@NonNull String source) {
+    public Directional convert(@NonNull String source) {
         try {
-            return Position.valueOf(source.toUpperCase());
+            return Directional.valueOf(source.toUpperCase());
         } catch (IllegalArgumentException e) {
             return null;
         }
